06e87ee50720f883b96f5cfb9206f292b3ff9782
Alexander van Vugt <alexandervanvugt@MacBook-Pro.localdomain>
2020-01-18 07:50:41 +0100

    update after live class Jan. 2020

--- kube-setup.sh
+++ kube-setup.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 #
-# verified on Fedora 29 Server
-
-
-# add vbox repo
-rm -f /etc/yum.repos.d/vbox.repo
-
-cat << REPO >> /etc/yum.repos.d/vbox.repo
-[virtualbox]
-name=Fedora $releasever - $basearch - VirtualBox
-baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/\$releasever/\$basearch
-enabled=1
-gpgcheck=0
-repo_gpgcheck=0
-gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
-REPO
+# verified on Fedora 31 WS
+egrep '^flags.*(vmx|svm)' /proc/cpuinfo || (echo enable CPU virtualization support and try again && exit 9)
 
 dnf clean all
-dnf upgrade
+dnf -y upgrade
 
-# install vbox
-echo installing virtualbox
-dnf install make perl kernel-devel gcc elfutils-libelf-devel -y
-dnf install VirtualBox-5.2 -y
+# install KVM software
+dnf install @virtualization -y
+systemctl enable --now libvirtd
+
+# install kubectl
 echo installing kubectl
-dnf install kubernetes-client -y
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+dnf install -y kubectl
+
+# install minikube
 echo downloading minikube, check version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 
 chmod +x minikube
-cp minikube /usr/local/bin
+mv minikube /usr/local/bin
 
-echo at this point, reboot your Fedora Server. After reboot, manually run:
-echo vboxconfig
-echo minikube start
+echo at this point, reboot your Fedora Workstation. After reboot, manually run as non-root
+echo minikube start --memory 4096 --vm-driver=kvm2
