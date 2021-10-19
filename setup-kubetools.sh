3417bc849e84d21670b46ff60455b93165872ea5
sandervanvugt <mail@sandervanvugt.nl>
2021-10-19 11:23:47 +0200

    message

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -20,14 +20,14 @@ if [ $MYOS = "CentOS" ]
 then
 	echo RUNNING CENTOS CONFIG
 	cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-	[kubernetes]
-	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-	enabled=1
-	gpgcheck=1
-	repo_gpgcheck=1
-	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-	exclude=kubelet kubeadm kubectl
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
 EOF
 
 	# Set SELinux in permissive mode (effectively disabling it)
