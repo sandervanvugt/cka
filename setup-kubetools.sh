384b5cdcf6e49225782ab93235c116d8f5b37f5f
sandervanvugt <mail@sandervanvugt.nl>
2021-10-19 11:14:28 +0200

    message

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -29,15 +29,6 @@ then
 	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 	exclude=kubelet kubeadm kubectl
 EOF
-	cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-	[kubernetes]
-	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-	enabled=1
-	gpgcheck=1
-	repo_gpgcheck=1
-	gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
 
 	# Set SELinux in permissive mode (effectively disabling it)
 	setenforce 0
