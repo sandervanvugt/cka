81e0773359cdb1f89b1b2cb5eeef404a182f4b2d
sandervanvugt <mail@sandervanvugt.nl>
2024-03-11 11:55:41 +0100

    message

--- setup-kubetools-previousversion.sh
+++ setup-kubetools-previousversion.sh
@@ -34,8 +34,8 @@ then
 EOF
 	
 	sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-	curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	curl -fsSL https://pkgs.k8s.io/core:/stable:/${PREVIOUSVERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${PREVIOUSVERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sleep 2
 
 	sudo apt-get update
