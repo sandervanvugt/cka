e41ead556113dd32c3378f29a525acaf304052e6
sandervanvugt <mail@sandervanvugt.nl>
2024-03-11 11:31:49 +0100

    message

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -37,7 +37,8 @@ EOF
 #EOF
 	sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 	curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sleep 2
 
 	sudo apt-get update
 	sudo apt-get install -y kubelet kubeadm kubectl
